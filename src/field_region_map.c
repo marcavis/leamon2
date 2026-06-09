8165cdc500c6e562e01b9dc2fca2ae729539619e
marcavis <marcavis@yahoo.com.br>
2026-06-09 01:56:17 -0300

    map fiddling

--- src/field_region_map.c
+++ src/field_region_map.c
@@ -76,7 +76,7 @@ static const struct WindowTemplate sFieldRegionMapWindowTemplates[] =
 {
     [WIN_MAPSEC_NAME] = {
         .bg = 0,
-        .tilemapLeft = 17,
+        .tilemapLeft = 1,
         .tilemapTop = 17,
         .width = 12,
         .height = 2,
--- src/region_map.c
+++ src/region_map.c
@@ -584,7 +584,7 @@ static const struct WindowTemplate sFlyMapWindowTemplates[] =
 {
     [WIN_MAPSEC_NAME] = {
         .bg = 0,
-        .tilemapLeft = 17,
+        .tilemapLeft = 1,
         .tilemapTop = 17,
         .width = 12,
         .height = 2,
@@ -593,7 +593,7 @@ static const struct WindowTemplate sFlyMapWindowTemplates[] =
     },
     [WIN_MAPSEC_NAME_TALL] = {
         .bg = 0,
-        .tilemapLeft = 17,
+        .tilemapLeft = 1,
         .tilemapTop = 15,
         .width = 12,
         .height = 4,
